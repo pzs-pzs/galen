24030335fa3cf3ea026ef22183eabedc412de865
puzhengsong <1391663722@qq.com>
2022-11-07 19:11:56 +0800

    :art: add some answer

--- tree/tree.go
+++ tree/tree.go
@@ -1,5 +1,7 @@
 package tree
 
+import "fmt"
+
 type TreeNode struct {
 	Val   int
 	Left  *TreeNode
@@ -22,4 +24,42 @@ func inorderTraversal(root *TreeNode) []int {
 	}
 	f(root)
 	return ans
+}
+
+func binaryTreePaths(root *TreeNode) []string {
+	if root == nil {
+		return nil
+	}
+	var ans []string
+	var f func(root *TreeNode, prefix string)
+	f = func(root *TreeNode, prefix string) {
+		if root.Left != nil {
+			f(root.Left, fmt.Sprintf("%s->%d", prefix, root.Left.Val))
+		}
+		if root.Right != nil {
+			f(root.Right, fmt.Sprintf("%s->%d", prefix, root.Right.Val))
+		}
+		if root.Left == nil && root.Right == nil {
+			ans = append(ans, prefix)
+		}
+	}
+	f(root, fmt.Sprintf("%d", root.Val))
+	return ans
+}
+
+func isSymmetric(root *TreeNode) bool {
+	if root == nil {
+		return false
+	}
+	var fn func(l, r *TreeNode) bool
+	fn = func(l, r *TreeNode) bool {
+		if l == nil && r == nil {
+			return true
+		}
+		if l == nil || r == nil {
+			return false
+		}
+		return l.Val == r.Val && fn(l.Left, r.Right) && fn(l.Right, r.Left)
+	}
+	return fn(root.Left, root.Right)
 }
