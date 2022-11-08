d9ee86a5f62456465313712fb92f7901ba5e73ea
puzhengsong <1391663722@qq.com>
2022-11-08 11:10:08 +0800

    :art: binary search

--- search/binary_search.go
+++ search/binary_search.go
@@ -15,3 +15,20 @@ func firstBadVersion(n int) int {
 	}
 	return l
 }
+
+func mySqrt(x int) int {
+	l, r := 1, x
+	for l <= r {
+		mid := (l + r) / 2
+		if mid*mid == x {
+			return mid
+		}
+		if mid*mid < x {
+			l = mid + 1
+		}
+		if mid*mid > x {
+			r = mid - 1
+		}
+	}
+	return r
+}
---
+++ search/binary_search_test.go
@@ -0,0 +1,8 @@
+package search
+
+import "testing"
+
+func Test_mySqrt(t *testing.T) {
+	sqrt := mySqrt(4)
+	println(sqrt)
+}
