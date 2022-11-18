e4d172fcfd8c0d68f626fd22f5c97da0b53790e0
puzhengsong <1391663722@qq.com>
2022-11-18 18:03:05 +0800

    :art: add some answers

--- doublepointer/two_sum.go
+++ doublepointer/two_sum.go
@@ -1,5 +1,7 @@
 package doublepointer
 
+import "sort"
+
 func twoSum(nums []int, target int) []int {
 	index := make(map[int]int)
 	for i, v := range nums {
@@ -88,4 +90,81 @@ func twoSum2(numbers []int, target int) []int {
 		}
 	}
 	return []int{-1, -1}
+}
+
+func numMatchingSubseq(s string, words []string) int {
+	var ans int
+	index := make(map[rune][]int)
+	for i, v := range s {
+		index[v] = append(index[v], i)
+	}
+	for _, word := range words {
+		lastIndex := -1
+		ok := true
+		for _, v := range word {
+			idxs := index[v]
+			l, r := 0, len(idxs)
+			for l < r {
+				mid := (l + r) / 2
+				if idxs[mid] >= lastIndex+1 {
+					r = mid
+				} else {
+					l = mid + 1
+				}
+			}
+			if r == len(idxs) {
+				ok = false
+				break
+			}
+			lastIndex = idxs[r]
+		}
+		if ok {
+			ans++
+		}
+
+	}
+	return ans
+}
+
+func isSubsequence(s string, t string) bool {
+	n, m := len(s), len(t)
+	i, j := 0, 0
+	for i < n && j < m {
+		if s[i] == t[j] {
+			i++
+		}
+		j++
+	}
+	return i == n
+}
+
+func distinctAverages(nums []int) int {
+	sort.Ints(nums)
+	l, r := 0, len(nums)-1
+	ans := map[float64]struct{}{}
+	for l < r {
+		ans[float64(nums[l]+nums[r])/float64(2)] = struct{}{}
+		l++
+		r--
+	}
+	return len(ans)
+}
+
+func topKFrequent(nums []int, k int) []int {
+	cnt := map[int]int{}
+	for _, num := range nums {
+		cnt[num] = cnt[num] + 1
+	}
+	var s [][]int
+	for i, c := range cnt {
+		s = append(s, []int{c, i})
+	}
+	sort.Slice(s, func(i, j int) bool {
+		return s[i][0] > s[j][0]
+	})
+	var ans []int
+	for i := 0; i < k; i++ {
+		ans = append(ans, s[i][1])
+	}
+	return ans
 }
--- doublepointer/two_sum_test.go
+++ doublepointer/two_sum_test.go
@@ -2,6 +2,7 @@ package doublepointer
 
 import (
 	"github.com/stretchr/testify/assert"
+	"sort"
 	"testing"
 )
 
@@ -13,4 +14,30 @@ func TestReverse(t *testing.T) {
 func TestTwoSum2(t *testing.T) {
 	rst := twoSum2([]int{2, 7, 11, 15}, 9)
 	assert.Equal(t, rst, []int{1, 2})
+}
+
+func Test_numMatchingSubseq(t *testing.T) {
+	numMatchingSubseq("abcde", []string{"a", "bb", "acd", "ace"})
+
+	nums := []int{1, 3, 3, 6, 6, 8}
+	i := sort.Search(len(nums), func(i int) bool {
+		return nums[i] >= 5
+	})
+
+	l, r := 0, len(nums)
+	var mid int
+	target := 3
+	for l < r {
+		mid = (l + r) / 2
+		if nums[mid] < target {
+			l = mid + 1
+		} else if nums[mid] > target {
+			r = mid
+		} else {
+			l = mid + 1
+		}
+
+	}
+	println(l - 1)
+	println(i)
 }
